1f243773567bf9bc0d4780133f2c5772552123f6
apple <apple@apples-MacBook-Pro.local>
2020-06-11 13:03:55 +0200

    added a few gems

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,4 +3,14 @@ class TagsController < ApplicationController
         @tag = Tag.find(params[:id])
       end
       
+      def index
+        @tags = Tag.all
+      end
+    
+      def destroy
+        @tag = Tag.find(params[:id])
+        @tag.destroy
+        flash.notice = "Tag '#{@tag.name}' has been deleted"
+        redirect_to tags_path
+      end
 end
